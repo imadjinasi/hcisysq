abd1a19961cc9c920e33babd46fcebce23b37d95
hcysq <humancapital.ysq@gmail.com>
2026-08-21 13:21:49 +0700

    chore: remove token path activation wrapper

--- apps/web/src/pages/AccountActivationRoutePage.tsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "@tanstack/react-router";
-
-import { AccountActivationPage } from "@/pages/AccountActivationPage";
-
-export function AccountActivationRoutePage() {
-  const { token } = useParams({ from: "/activate/$token" });
-  return <AccountActivationPage token={token} />;
-}
